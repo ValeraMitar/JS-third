bc8f7783a1bbe76fe13002d07f94616ef1999fcc
ValeraMitar <mitar91@mail.ru>
2017-03-19 23:37:50 +0300

    леша посмотри этот вариант ты так сделал?

--- app.js
+++ app.js
@@ -3,13 +3,16 @@ window.addEventListener('load', function () {
     function spanClickListener(event) {
         var target = event.target;
         var onClick = document.querySelector('.active');
+        /*onClick.classList.remove('active');*/
         if (target.matches(' p span')) { /*or (span.tagName === 'SPAN'*/
             target.classList.toggle('active');
+            onClick.classList.remove('active');
             /*target.addEventListener('transitionend', transitionEndListener);*/
             console.log(onClick);
-            if (onClick !== target) {
-                onClick.classList.remove('active');
-            }
+            /*if (onClick !== target) {
+                onClick.classList.add('active');
+                console.log(onClick);
+            }*/
         }
     }
 /*    function transitionEndListener() {
@@ -30,3 +33,6 @@ function transitionEndListener(event) {
     span.classList.remove('active');
 
 }*/
+/*Только отличие было в том что я явно удалял класс у найденного элемента, а переключал у того, на который кликнул
+ У тебя наоборот
+ Но оно и так работает*/
